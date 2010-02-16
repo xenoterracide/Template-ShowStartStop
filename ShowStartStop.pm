53a93a9762cc2d21c79b21f2aa22890d4b603187
Caleb Cushing <xenoterracide@gmail.com>
2010-02-15 22:14:08 -0500

    Clean up files for wrong comments

--- ShowStartStop.pm
+++ ShowStartStop.pm
@@ -9,11 +9,11 @@ Template::ShowStartStop - Display where template's start and stop
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
